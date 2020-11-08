82d2dfb0ec866a86a32ca760c603de7e46c7926d
Samuel Kupoe <samuelkupoe1@gmail.com>
2020-11-08 22:48:51 +0000

    Frontend of a Profile

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, Text, View, Image } from 'react-native';
+import profile from './assets/profile.jpg'
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  
+      <Image source={profile} style={styles.image}/>
+
+      <View style={styles.infoContainer}>
+      <Text style={styles.label}>School</Text>
+      <Text style={styles.info}>The Lawrenceville School</Text>
+
+      <Text style={styles.label}>Email Address</Text>
+      <Text style={styles.info}>danielrozar@gmail.com</Text>
+
+      <Text style={styles.label2}>Name</Text>
+      <Text style={styles.info2}>Daniel Rozar</Text>
+
+      <Text style={styles.label2}>Nick Name</Text>
+      <Text style={styles.info2}>r.daniel</Text>
+
+      <Text style={styles.label2}>Emergency Contact</Text>
+      <Text style={styles.info2}>+1-987654321</Text>
+      </View>
+
+      
+      <TouchableOpacity  style={styles.button}>
+      
+      <Text style={styles.buttonText}>UPDATE PROFILE</Text>
+
+    </TouchableOpacity>
+     
+
     </View>
   );
 }
@@ -14,8 +40,53 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  image: {
+    width:150,
+    height:150,
+    alignSelf:"center",
+    marginTop:100,
+    borderRadius:75
+  },
+infoContainer: {
+  marginTop:20,
+  flexDirection:"column",
+  marginHorizontal:50
+},
+label:{
+  fontSize:20,
+  color:"lightgray",
+  marginTop:15
+},
+info: {
+  fontSize:25,
+  marginTop:5
+},
+label2:{
+  fontSize:20,
+  color:"skyblue",
+  marginTop:20
+},
+info2:{
+  fontSize:25,
+  borderBottomColor:"lightgray",
+  borderBottomWidth:0.8
+},
+button:{
+  backgroundColor:"#66B2FF",
+  width: 300,
+  height: 50,
+  alignSelf:"center",
+  marginTop:30,
+  borderRadius:25
+},
+buttonText:{
+  fontSize:20,
+  alignSelf:"center",
+  paddingTop:12,
+  color:"white"
+}
+
 });
+
+export default App;
